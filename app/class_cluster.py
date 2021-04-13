703aa35e78030cd5f07540ef66064be3b7256bfa
Frank UN <frankuchenna30@gmail.com>
2021-04-13 22:43:47 +0100

    added clean_post func and tidy_post func

--- app/class_cluster.py
+++ app/class_cluster.py
@@ -1,7 +1,23 @@
 import pandas as pd
 import re
+import spacy
+import en_core_web_sm
+from nltk.tokenize import RegexpTokenizer, WhitespaceTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import string
+
 from app import db
 from app.models import Post
+
+
+nlp = en_core_web_sm.load()
+tokenizer = RegexpTokenizer(r'\w+')
+lemmatizer = WordNetLemmatizer()
+stop_word = set(stopwords.words('english'))
+punctuate = list(string.punctuation)
+stop_word.update(punctuate)
+word_tokenizer = WhitespaceTokenizer() 
 
 
 def post_to_df():
@@ -40,8 +56,7 @@ def clean_post(df, text):
 	print('post_df.lower()\n', df[text])
 	
 	#Remove all unnecessary characters
-	df[text] = df[text].apply(lambda elem: 
-			re.sub(r"(@[A-Za-z0–9]+)|([0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?",
+	df[text] = df[text].apply(lambda elem: re.sub(r"(@[A-Za-z0–9]+)|([0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?",
 			"",
 			elem))
 
@@ -50,3 +65,20 @@ def clean_post(df, text):
 post_df = clean_post(post_df, 'content')
 
 print('Cleaned Post\n', post_df.head())
+
+
+def tidy_post(text):
+	"""
+	This function will Tokenize posts splitting string into list
+	remove stop words and reduce words to their root form by lemmatization 
+	"""
+	final_text = []
+	for i in word_tokenizer.tokenize(text):
+		if i.lower() not in stop_word:
+			word = lemmatizer.lemmatize(i)
+			final_text.append(word.lower())
+
+	return " ".join(final_text)
+
+post_df.content =post_df.content.apply(tidy_post)
+print('post_df.content\n', post_df.content)
