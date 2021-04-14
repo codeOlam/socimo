6d5dacd46ceda8ac8eca52b536158d5838fa6f29
Frank UN <frankuchenna30@gmail.com>
2021-04-14 08:51:41 +0100

    added setwords for cluster point

---
+++ app/setwords.py
@@ -0,0 +1,11 @@
+health_related_words = '''covid19 life death recover fever cough hospital disease ill'''
+
+
+politics_related_words = '''PDP APC party senator president local government asorock asovilla abuja
+							election appointment permsec rally campaign'''
+
+
+security_related_words = '''bandits gunmen kidnap killed attacked'''
+
+
+economic_related_words = '''banks CBN cbn money naira dollar btc bitcion crypto wallet budget'''
