7b3732c8b7ffdd5c4a82f803069be20286f74207
Frank UN <frankuchenna30@gmail.com>
2021-04-14 17:47:10 +0100

    added to stopwards

--- app/setwords.py
+++ app/setwords.py
@@ -1,11 +1,17 @@
-health_related_words = '''covid19 life death recover fever cough hospital disease ill'''
+health_related_words = '''covid19 life death recover fever cough hospital disease ill virus'''
 
 
 politics_related_words = '''PDP APC party senator president local government asorock asovilla abuja
-							election appointment permsec rally campaign'''
+							election appointment permsec rally campaign policy country capital vote
+							foriegn territory'''
 
 
-security_related_words = '''bandits gunmen kidnap killed attacked'''
+security_related_words = '''bandits gunmen kidnap killed attacked robbery cultist gunshot stabbing
+							armedmen herdsmen bokoharam military hostage captive captor criminal
+							arrest arrested'''
 
 
-economic_related_words = '''banks CBN cbn money naira dollar btc bitcion crypto wallet budget'''
+economic_related_words = '''banks CBN cbn money fund naira dollar btc bitcion crypto cash 
+							liquidated transfer wired buy sell stockmarket stock exchange wallet 
+							budget transfer account currency trading rate centralbanks wto worldbank
+							sec finance investment investor housing'''
