19e25543fe349f87c74a745c4286788542125745
Frank UN <frankuchenna30@gmail.com>
2021-04-25 19:20:42 +0100

    modified

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     CSRF_ENABLED = True
     # SECRET_KEY = 'nkajbvairskjnobrijbvpairjb98ryhvpq3u4h-q98yrg' #used for dev only
     SECRET_KEY = os.environ.get('SECRET_KEY') # for production
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://pos,tgres:''@localhost:5432/social_community' #'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:''@localhost:5432/social_community' #'sqlite:///' + os.path.join(basedir, 'app.db')
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') #'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
